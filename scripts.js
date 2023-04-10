5ca92c52556fd3b8846aa74d5e495b56ddce565c
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2023-04-10 18:21:29 -0300

    Projeto iphone

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocacor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaimagem(imagem) {
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
